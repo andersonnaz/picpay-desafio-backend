ca4b179ba3e0b77ca12830782a488eaf7179fcd8
andersonnaz <anderson_1004@hotmail.com>
2023-12-02 11:44:21 -0300

    feat: add AddCustomerRepository protocol

---
+++ src/data/protocols/db/customer/add-customer-repository.ts
@@ -0,0 +1,22 @@
+export interface AddCustomerRepository {
+    add(customer: AddCustomerRepository.Params): Promise<AddCustomerRepository.Result>
+}
+
+export namespace AddCustomerRepository {
+    export type Params = {
+        name: string
+        email: string
+        password: string
+        cpf: string
+        accessToken: string
+    }
+
+    export type Result = {
+        id: string
+        name: string
+        email: string
+        password: string
+        cpf: string
+        accessToken: string
+    } | undefined
+}
