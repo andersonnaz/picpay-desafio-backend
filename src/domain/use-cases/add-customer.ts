651af222412926dc1fd3b7afa9393a249027507f
andersonnaz <anderson_1004@hotmail.com>
2023-12-02 11:50:01 -0300

    feat: add AddCustomer use case

---
+++ src/domain/use-cases/add-customer.ts
@@ -0,0 +1,30 @@
+import { HashGenerator } from "../../data/cryptography/hash-generator"
+import { AddCustomerRepository } from "../../data/protocols/db/customer/add-customer-repository"
+
+export interface AddCustomer {
+    add(customerData: AddCustomer.Params): Promise<AddCustomer.Result>
+}
+
+export namespace AddCustomer {
+    export type Params = {
+        name: string
+        email: string
+        password: string
+        cpf: string
+        accessToken: string
+    }
+
+    export type Result = {
+        id: string
+        name: string
+        email: string
+        password: string
+        cpf: string
+        accessToken: string
+    } | undefined
+
+    export type Dependencies = {
+        customerRepository: AddCustomerRepository
+        hashService: HashGenerator
+    }
+}
