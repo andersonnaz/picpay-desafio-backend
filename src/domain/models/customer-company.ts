350a8b80717f41e597f07118b5276303412f1c98
andersonnaz <anderson_1004@hotmail.com>
2023-12-02 09:57:05 -0300

    feat: add entities

---
+++ src/domain/models/customer-company.ts
@@ -0,0 +1,7 @@
+export interface CustomerCompany {
+    name: string
+    email: string
+    password: string
+    cnpj: string
+    accessToken: string | null
+}
---
+++ src/domain/models/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+    name: string
+    email: string
+    password: string
+    cpf: string
+    accessToken: string | null
+}
