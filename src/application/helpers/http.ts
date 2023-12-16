680a87a04c3a4bdd1356a9b6481431aa8a5a386a
andersonnaz <anderson_1004@hotmail.com>
2023-12-16 08:43:30 -0300

    feat: add http response methods

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -8,3 +8,28 @@ export interface HttpResponse {
     statusCode: number
     body?: any
 }
+
+export const success = (data: any): HttpResponse => ({
+    statusCode: 200,
+    body: data
+})
+
+export const create = (data: any): HttpResponse => ({
+    statusCode: 201,
+    body: data
+})
+
+export const badRequest = (error: Error): HttpResponse => ({
+    statusCode: 400,
+    body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+    statusCode: 500,
+    body: error
+})
+
+export const conflict = (error: Error): HttpResponse => ({
+    statusCode: 409,
+    body: error
+})
