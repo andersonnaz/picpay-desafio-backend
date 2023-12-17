b9153ce43a0180f1877c398880ddb436209ab08e
andersonnaz <anderson_1004@hotmail.com>
2023-12-17 10:00:10 -0300

    feat: ensure AddCustomerCompany returns 409 (conflict) if customerCompany already exists

---
+++ src/application/controllers/add-customer-company.ts
@@ -0,0 +1,30 @@
+import { AddCustomerCompany } from "../../domain/use-cases/add-customer-company";
+import { ConflictCustomerError } from "../errors/conflict-customer-error";
+import { HttpRequest, HttpResponse, conflict } from "../helpers/http";
+import { Controller } from "../protocols/controller";
+
+export class AddCustomerCompanyController implements Controller {
+    private readonly addCustomerCompany: AddCustomerCompany
+
+    constructor({addCustomerCompany}: AddCustomerCompanyController.Dependencies){
+        this.addCustomerCompany = addCustomerCompany
+    }
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { name, email, password, cnpj, accessToken } = httpRequest.body
+            const customerCompany = await this.addCustomerCompany.add({name, email, password, cnpj, accessToken})
+            if(!customerCompany){
+                return conflict(new ConflictCustomerError())
+            }
+        } catch (error) {
+
+        }
+    }
+}
+
+export namespace AddCustomerCompanyController {
+    export type Dependencies = {
+        addCustomerCompany: AddCustomerCompany
+    }
+}
