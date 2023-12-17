29bfcf9def5841eb27a72564b8b6aedd2121e14f
andersonnaz <anderson_1004@hotmail.com>
2023-12-17 10:00:24 -0300

    test: ensure AddCustomerCompany returns 409 (conflict) if customerCompany already exists

---
+++ tests/src/application/controllers/add-customer-company.spec.ts
@@ -0,0 +1,56 @@
+import { AddCustomerCompanyController } from "../../../../src/application/controllers/add-customer-company";
+import { ConflictCustomerError } from "../../../../src/application/errors/conflict-customer-error";
+import { HttpRequest, conflict } from "../../../../src/application/helpers/http";
+import { AddCustomerCompany } from "../../../../src/domain/use-cases/add-customer-company";
+
+const makeAddCustomerCompany = (): AddCustomerCompany => {
+    class AddCustomerCompanyStub implements AddCustomerCompany {
+        async add(customerData: AddCustomerCompany.Params): Promise<AddCustomerCompany.Result> {
+            return {
+                id: 'any_id',
+                name: 'any_name',
+                email: 'any_email',
+                cnpj: 'any_cnpj',
+                accessToken: 'any_token'
+            }
+        }
+    }
+    return new AddCustomerCompanyStub()
+}
+
+interface SutTypes {
+    sut: AddCustomerCompanyController
+    addCustomerCompanyStub: AddCustomerCompany
+}
+
+const makeSut = (): SutTypes => {
+    const addCustomerCompanyStub = makeAddCustomerCompany()
+    const dependencies: AddCustomerCompanyController.Dependencies = {
+        addCustomerCompany: addCustomerCompanyStub
+    }
+    const sut = new AddCustomerCompanyController(dependencies)
+    return {
+        sut,
+        addCustomerCompanyStub
+    }
+}
+
+describe('AddCustomerCompany Controller', () => {
+    const fakeBodyHttpRequest = {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        cnpj: 'any_cnpj',
+        accessToken: 'any_token'
+    }
+
+    const fakeHttpRequest: HttpRequest = {
+        body: fakeBodyHttpRequest
+    }
+    test('should return 409 (conflit) if addCustomerCompany returns undefined', async () => {
+        const { sut, addCustomerCompanyStub } = makeSut()
+        jest.spyOn(addCustomerCompanyStub, 'add').mockReturnValueOnce(undefined)
+        const httpResponse = await sut.handle(fakeHttpRequest)
+        expect(httpResponse).toEqual(conflict(new ConflictCustomerError()))
+    })
+});
